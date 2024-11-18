d9a926d57818f413323710bf67595cbb25e78d59
Jamer Sanchez Valiente <svjamer@gmail.com>
2024-11-18 01:55:06 -0500

    Implementacion de Venta con Usuario

--- src/venta/entities/venta.entity.ts
+++ src/venta/entities/venta.entity.ts
@@ -1,15 +1,17 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Types } from "mongoose";
 
+export enum MetodoPago{
+  Tarjeta = "Tarjeta",
+}
 @Schema()
 export class Venta extends Document{
 
-  //@Prop({type: Types.ObjectId, ref: 'Usuario', required: true})
-  //usuario: Types.ObjectId
+  @Prop({type: Types.ObjectId, ref: 'usuario', required: true})
+  usuario: Types.ObjectId
 
   @Prop({ required: true, default: () => new Date() })
   fecha: Date; 
-
 
   @Prop({required: true})
   total: number;
