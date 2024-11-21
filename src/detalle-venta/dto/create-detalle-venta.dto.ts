368892d761d4da58f90de1968964490a560fe952
Cabanillas <kaorycabanillas01@gmail.com>
2024-11-20 22:51:24 -0500

    Correción de DetVent e implementación Crud

--- src/detalle-venta/dto/create-detalle-venta.dto.ts
+++ src/detalle-venta/dto/create-detalle-venta.dto.ts
@@ -1,21 +1,16 @@
-import { Type } from 'class-transformer';
-import { IsInt, IsDecimal, Min, IsNotEmpty, ValidateNested, IsArray, ArrayNotEmpty, IsMongoId, IsNumber } from 'class-validator';
-import { CreateProductoDto } from 'src/productos/dto/create-producto.dto';
-import { Producto } from 'src/productos/entities/producto.entity';
-
+import { IsInt, Min, IsNotEmpty, IsNumber, IsString, IsOptional,  } from 'class-validator';
 export class CreateDetalleVentaDto {
 
   @IsInt()
   @Min(1)
   cantidadprod: number;
  
+  @IsOptional()
   @IsNumber({maxDecimalPlaces: 2})
-  @Min(1)
   subtotal: number;
 
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsMongoId({ each: true })
-  productosAsociados: string[];
+  @IsNotEmpty()
+  @IsString()
+  idproducto: string;
 
 }
